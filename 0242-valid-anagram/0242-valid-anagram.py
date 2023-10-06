bfcb0cfaa5a3b972eb5d6f1f02b7a2999ca49444
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2023-10-06 09:37:56 +0530

    Time: 57 ms (43.08%), Space: 16.7 MB (95.00%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        s_map,t_map = defaultdict(int), defaultdict(int)
+        if len(s) != len(t):
+            return False
+        for ch1,ch2 in zip(s,t):
+            s_map[ch1]+=1
+            t_map[ch2]+=1
+        return s_map == t_map
