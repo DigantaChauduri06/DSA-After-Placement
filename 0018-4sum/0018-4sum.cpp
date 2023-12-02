895876e8065b23dc3bd0fad451a681989c5a1532
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2023-12-02 17:24:54 +0530

    Time: 1519 ms (5.40%), Space: 13.7 MB (55.79%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         for (int i = 0;i < n-3;i++) {
             for (int j = i+1;j < n-2;j++) {
                 for (int k = j+1;k < n-1;k++) {
@@ -12,7 +12,7 @@ public:
                         int m = (st + ed) / 2;
                         long long sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[k] + (long long)nums[m];
                         if (sum == target) {
-                            ans.insert({nums[i], nums[j], nums[k], nums[m]});
+                            ans.push_back({nums[i], nums[j], nums[k], nums[m]});
                             break;
                         }
                         else if (sum > target) {
@@ -21,10 +21,12 @@ public:
                             st++;
                         }
                     }
+                    while (k < n - 1 && nums[k] == nums[k+1]) k+=1;
                 }
-                
+                while (j < n - 1 && nums[j] == nums[j+1]) j+=1;
             }
+            while (i < n - 1 && nums[i] == nums[i+1]) i+=1;
         }
-        return vector<vector<int>>(ans.begin(), ans.end());
+        return ans;
     }
 };
